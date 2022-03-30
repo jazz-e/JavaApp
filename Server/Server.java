7ae28f43fe4a3087c76dbe161298b149bf5af8c0
jazze <dead__or__alive@hotmail.com>
2022-03-30 19:12:57 +0100

    Complete

--- Server/Server.java
+++ Server/Server.java
@@ -1,5 +1,23 @@
-public class Server{
-    public static void main(String[] args){
-    
+import java.net.*;
+import java.io.*;
+
+public class server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = ss.accept();
+
+        System.out.println("client conneted");
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+
+        System.out.println("client : " + str);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+
+        pr.println("Yes!");
+        pr.flush();
     }
 }
